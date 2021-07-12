2c4d218f64f0bd12f3f7b4619fe52887694fa428
Robert Smith <robertsmith@Roberts-Mac.local>
2021-07-12 19:24:06 +0100

    ffs

--- kivy_ios/recipes/faiss/__init__.py
+++ kivy_ios/recipes/faiss/__init__.py
@@ -7,18 +7,18 @@ import os
 class FaissRecipe(PythonRecipe):
     version = "v1.7.1"
     url = "https://github.com/facebookresearch/faiss/archive/refs/tags/{version}.tar.gz"
-    depends = ["python", "numpy"]
+    depends = ["python"]
     include_per_arch = True
-    library = "libfaisscpu.a"
     libraries = [
-        "build/faiss/python/libfaiss.a",
+        "build/faiss/libfaiss.a",
     ]
 
     def build_arch(self, arch):
         build_env = arch.get_env()
-        command = sh.Command(join(self.build_dir, "cmake"))
-        shprint(command, "-B", "build", ".", "-DBUILD_SHARED_LIBS=OFF")
-        command = sh.Command(join(self.build_dir, "make"))
+        command = sh.Command("cmake")
+        shprint(command, "-B", "build", ".", "-DFAISS_ENABLE_GPU=OFF",
+                "-DBUILD_SHARED_LIBS=OFF", f"-DCMAKE_OSX_ARCHITECTURES={arch}")
+        command = sh.Command("make")
         shprint(command, "-C", "build", "-j", "faiss")
         shprint(command, "-C", "build", "-j", "swigfaiss")
         super(FaissRecipe, self).build_arch(arch)
@@ -31,7 +31,13 @@ class FaissRecipe(PythonRecipe):
         build_env = arch.get_env()
         dest_dir = join(self.ctx.dist_dir, "root", "python3")
         build_env['PYTHONPATH'] = self.ctx.site_packages_dir
-        shprint(hostpython, "setup.py", "install", "--prefix", dest_dir, _env=build_env)
+        shprint(hostpython,
+                "setup.py",
+                "install",
+                "--prefix",
+                dest_dir,
+                _env=build_env)
 
 
 recipe = FaissRecipe()
+
