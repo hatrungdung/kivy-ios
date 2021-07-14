e25f3906e3140ff2b72910563baf18c6e3a487e2
Robert Smith <robertsmith@Roberts-Mac.local>
2021-07-14 16:20:01 +0100

    ffs

--- kivy_ios/recipes/faiss/__init__.py
+++ kivy_ios/recipes/faiss/__init__.py
@@ -1,4 +1,5 @@
 # https://mac.r-project.org/openmp/
+# https://mac.r-project.org/openmp/openmp-96efe90-darwin20-Release.tar.gz
 
 from kivy_ios.toolchain import PythonRecipe, shprint
 from os.path import join
@@ -17,12 +18,31 @@ class FaissRecipe(PythonRecipe):
 
     def build_arch(self, arch):
         build_env = arch.get_env()
+        build_env['CXXFLAGS'] = build_env.get(
+            'CXXFLAGS', ''
+        ) + " -I{self.build_dir}/../../../../../../../../usr/local/include"
+        build_env['LDFLAGS'] = build_env.get(
+            'LDFLAGS', ''
+        ) + " -L/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.framework -lBLAS -lLAPACK"
         command = sh.Command("cmake")
-        shprint(command, "-B", "build", ".", "-DFAISS_ENABLE_GPU=OFF",
-                "-DBUILD_SHARED_LIBS=OFF", f"-DCMAKE_OSX_ARCHITECTURES={arch}")
+        shprint(
+            command,
+            "-Bbuild",
+            "-S.",
+            "-DFAISS_ENABLE_GPU=OFF",
+            "-DBUILD_SHARED_LIBS=OFF",
+            "-DCMAKE_SYSTEM_NAME=iOS",
+            f"-DCMAKE_OSX_ARCHITECTURES={arch}",
+            f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
+            "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
+            "-DCMAKE_CXX_COMPILER_ID=AppleClang",
+            "-DOpenMP_CXX_FLAGS=-Xclang -fopenmp",
+            "-DOpenMP_CXX_LIB_NAMES=omp",
+            f"-DOpenMP_omp_LIBRARY={self.build_dir}/../../../../../../../../usr/local/lib/libomp.dylib",
+            _env=build_env)
         command = sh.Command("make")
-        shprint(command, "-C", "build", "-j", "faiss")
-        shprint(command, "-C", "build", "-j", "swigfaiss")
+        shprint(command, "-C", "build", "-j", "faiss", _env=build_env)
+        shprint(command, "-C", "build", "-j", "swigfaiss", _env=build_env)
         super(FaissRecipe, self).build_arch(arch)
 
     def install(self):
@@ -42,4 +62,3 @@ class FaissRecipe(PythonRecipe):
 
 
 recipe = FaissRecipe()
-
