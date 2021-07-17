989d19c5514ea42e618349524c38935acaf559d7
Robert Smith <robertsmith@Roberts-Mac.local>
2021-07-17 01:14:56 +0100

    try this

--- kivy_ios/recipes/faiss/__init__.py
+++ kivy_ios/recipes/faiss/__init__.py
@@ -17,7 +17,7 @@ class FaissRecipe(PythonRecipe):
     depends = ["python", "numpy", "openmp"]
     include_per_arch = True
     libraries = [
-        "build/faiss/libfaiss.a",
+        "_build/faiss/libfaiss.a", ####
     ]
 
     def build_arch(self, arch):
@@ -32,8 +32,8 @@ class FaissRecipe(PythonRecipe):
         numpy_dir = join(self.ctx.build_dir, 'numpy', arch.arch,
                          'numpy-1.20.2')  ###
         numpy_include = join(numpy_dir, 'build',
-                             f'src.macosx-10.15-x86_64-3.9', 'numpy',
-                             'core', 'include', 'numpy')
+                             f'src.macosx-10.15-x86_64-3.9', 'numpy', 'core',
+                             'include', 'numpy')
         numpy_include_dir = join(numpy_dir, 'numpy', 'core', 'include')
 
         build_env = arch.get_env()
@@ -43,12 +43,38 @@ class FaissRecipe(PythonRecipe):
 
         command = sh.Command("cmake")
         shprint(command,
-                "-Bbuild",
+                "-B_build",
                 "-S.",
                 "-DFAISS_ENABLE_GPU=OFF",
                 "-DBUILD_SHARED_LIBS=OFF",
                 "-DCMAKE_SYSTEM_NAME=iOS",
                 "-DCMAKE_SYSTEM_NAME=Darwin",
+                "-DMAKER_BUILD_TYPE=Release",
+                f"-DCMAKE_OSX_ARCHITECTURES={arch}",
+                f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
+                "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
+                "-DCMAKE_CXX_COMPILER_ID=AppleClang",
+                "-DOpenMP_CXX_FLAGS=-Xclang -fopenmp",
+                "-DOpenMP_CXX_LIB_NAMES=omp",
+                f"-DOpenMP_omp_LIBRARY={openmp_lib}",
+                "-DBLA_VENDOR=Apple",
+                f"-DFAISS_ENABLE_PYTHON=OFF",
+                _env=build_env)
+        command = sh.Command("make")
+        shprint(command, "-C", "_build", "-j", "faiss", _env=build_env)
+        command = sh.Command("cmake")
+        shprint(command, "--install", "_build", "--prefix", "_libfaiss_stage", _env=build_env)
+
+        command = sh.Command("cmake")
+        shprint(command,
+                "-B_build_python",
+                "-Sfaiss/python",
+                "-Dfaiss_ROOT=_libfaiss_stage",
+                "-DFAISS_ENABLE_GPU=OFF",
+                "-DBUILD_SHARED_LIBS=OFF",
+                "-DCMAKE_SYSTEM_NAME=iOS",
+                "-DCMAKE_SYSTEM_NAME=Darwin",
+                "-DMAKER_BUILD_TYPE=Release",
                 f"-DCMAKE_OSX_ARCHITECTURES={arch}",
                 f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
                 "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
@@ -62,27 +88,32 @@ class FaissRecipe(PythonRecipe):
                 f"-DPython_INCLUDE_DIR={python_include}",
                 f"-DPython_NumPy_INCLUDE_DIR={numpy_include_dir}",
                 _env=build_env)
-
         command = sh.Command("make")
-        shprint(command, "-C", "build", "-j", "faiss", _env=build_env)
-        shprint(command, "-C", "build", "-j", "swigfaiss", _env=build_env)
+        shprint(command, "-C", "_build_python", "-j", "swigfaiss", _env=build_env)
 
         super(FaissRecipe, self).build_arch(arch)
 
     def install(self):
-        arch = list(self.filtered_archs)[0]
-        build_dir = self.get_build_dir(arch.arch)
-        os.chdir(join(build_dir, "build", "faiss", "python"))
+        arch = self.filtered_archs[0]
+        name = self.name
+        env = self.get_recipe_env(arch)
+        build_dir = join(self.get_build_dir(arch.arch), "_build_python")
+        os.chdir(build_dir)
         hostpython = sh.Command(self.ctx.hostpython)
-        build_env = arch.get_env()
-        dest_dir = join(self.ctx.dist_dir, "root", "python3")
-        build_env['PYTHONPATH'] = self.ctx.site_packages_dir
-        shprint(hostpython,
-                "setup.py",
-                "install",
-                "--prefix",
-                dest_dir,
-                _env=build_env)
+
+        shprint(
+            hostpython,
+            "setup.py",
+            "install",
+            "--single-version-externally-managed",
+            "--record=record.txt",
+            "-O2",
+            "--root",
+            self.ctx.python_prefix,
+            "--prefix",
+            "",
+            _env=env,
+        )
 
 
 recipe = FaissRecipe()
