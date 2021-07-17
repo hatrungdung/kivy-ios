0683a3df8e58d909aa90b81b073f0a2ba5802482
Robert Smith <robertsmith@Roberts-Mac.local>
2021-07-17 17:49:08 +0100

    hum

--- kivy_ios/recipes/faiss/__init__.py
+++ kivy_ios/recipes/faiss/__init__.py
@@ -19,7 +19,7 @@ class FaissRecipe(PythonRecipe):
     include_per_arch = True
     libraries = [
         join("_libfaiss_stage", "lib", "libfaiss.a"),
-#        join("_libfaiss_stage", "lib", "libfaiss_avx2.a"),
+        # join("_libfaiss_stage", "lib", "libfaiss_avx2.a"),
     ]
 
     def build_arch(self, arch):
@@ -43,36 +43,46 @@ class FaissRecipe(PythonRecipe):
             'CXXFLAGS',
             '') + f" -Dnil=nil -std=c++11 -I{openmp_dir} -I{numpy_include}"
 
+        dest_dir = join(self.ctx.dist_dir, "root", "python")
+
         command = sh.Command("cmake")
-        shprint(command,
-                "-B_build",
-                "-S.",
-#                "-DFAISS_OPT_LEVEL=avx2",
-                "-DFAISS_ENABLE_GPU=OFF",
-                "-DBUILD_SHARED_LIBS=OFF",
-                "-DCMAKE_SYSTEM_NAME=iOS",
-                "-DCMAKE_SYSTEM_NAME=Darwin",
-                "-DMAKE_BUILD_TYPE=Release",
-                "-DCMAKE_INSTALL_LIBDIR=lib",
-                f"-DCMAKE_OSX_ARCHITECTURES={arch}",
-                f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
-                "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
-                "-DCMAKE_CXX_COMPILER_ID=AppleClang",
-                "-DOpenMP_CXX_FLAGS=-Xclang -fopenmp",
-                "-DOpenMP_CXX_LIB_NAMES=omp",
-                f"-DOpenMP_omp_LIBRARY={openmp_lib}",
-                "-DBLA_VENDOR=Apple",
-                f"-DFAISS_ENABLE_PYTHON=OFF",
-                _env=build_env)
+        shprint(
+            command,
+            "-B_build",
+            "-S.",
+            # "-DFAISS_OPT_LEVEL=avx2",
+            "-DFAISS_ENABLE_GPU=OFF",
+            "-DBUILD_SHARED_LIBS=OFF",
+            "-DCMAKE_SYSTEM_NAME=iOS",
+            "-DCMAKE_SYSTEM_NAME=Darwin",
+            "-DCMAKE_BUILD_TYPE=Release",
+            "-DCMAKE_INSTALL_LIBDIR=lib",
+            f"-DCMAKE_OSX_ARCHITECTURES={arch}",
+            f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
+            "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
+            "-DCMAKE_CXX_COMPILER_ID=AppleClang",
+            "-DOpenMP_CXX_FLAGS=-Xclang -fopenmp",
+            "-DOpenMP_CXX_LIB_NAMES=omp",
+            f"-DOpenMP_omp_LIBRARY={openmp_lib}",
+            "-DBLA_VENDOR=Apple",
+            f"-DFAISS_ENABLE_PYTHON=OFF",
+            _env=build_env)
         command = sh.Command("make")
-        shprint(command,
-                "-C",
-                "_build",
-                "-j",
-                "faiss",
-#                "faiss_avx2",
-                _env=build_env)
+        shprint(
+            command,
+            "-C",
+            "_build",
+            "-j",
+            "faiss",
+            # "faiss_avx2",
+            _env=build_env)
         command = sh.Command("cmake")
+        shprint(command,
+                "--install",
+                "_build",
+                "--prefix",
+                dest_dir,
+                _env=build_env)
         shprint(command,
                 "--install",
                 "_build",
@@ -81,61 +91,58 @@ class FaissRecipe(PythonRecipe):
                 _env=build_env)
 
         command = sh.Command("cmake")
-        shprint(command,
-                "-B_build_python",
-                "-Sfaiss/python",
-                "-Dfaiss_ROOT=_libfaiss_stage",
-#                "-DFAISS_OPT_LEVEL=avx2",
-                "-DFAISS_ENABLE_GPU=OFF",
-                "-DBUILD_SHARED_LIBS=OFF",
-                "-DCMAKE_SYSTEM_NAME=iOS",
-                "-DCMAKE_SYSTEM_NAME=Darwin",
-                "-DMAKE_BUILD_TYPE=Release",
-                f"-DCMAKE_OSX_ARCHITECTURES={arch}",
-                f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
-                "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
-                "-DCMAKE_CXX_COMPILER_ID=AppleClang",
-                "-DOpenMP_CXX_FLAGS=-Xclang -fopenmp",
-                "-DOpenMP_CXX_LIB_NAMES=omp",
-                f"-DOpenMP_omp_LIBRARY={openmp_lib}",
-                "-DBLA_VENDOR=Apple",
-                f"-DPython_EXECUTABLE={self.ctx.hostpython}",
-                f"-DPython_LIBRARY={python_lib}",
-                f"-DPython_INCLUDE_DIR={python_include}",
-                f"-DPython_NumPy_INCLUDE_DIR={numpy_include_dir}",
-                _env=build_env)
+        shprint(
+            command,
+            "-B_build_python",
+            "-Sfaiss/python",
+            "-Dfaiss_ROOT=_libfaiss_stage",
+            # "-DFAISS_OPT_LEVEL=avx2",
+            "-DFAISS_ENABLE_GPU=OFF",
+            "-DBUILD_SHARED_LIBS=OFF",
+            "-DCMAKE_SYSTEM_NAME=iOS",
+            "-DCMAKE_SYSTEM_NAME=Darwin",
+            "-DCMAKE_BUILD_TYPE=Release",
+            f"-DCMAKE_OSX_ARCHITECTURES={arch}",
+            f"-DCMAKE_OSX_SYSROOT={arch.sysroot}",
+            "-DCMAKE_CXX_COMPILER=/usr/bin/clang++",
+            "-DCMAKE_CXX_COMPILER_ID=AppleClang",
+            "-DOpenMP_CXX_FLAGS=-Xclang -fopenmp",
+            "-DOpenMP_CXX_LIB_NAMES=omp",
+            f"-DOpenMP_omp_LIBRARY={openmp_lib}",
+            "-DBLA_VENDOR=Apple",
+            f"-DPython_EXECUTABLE={self.ctx.hostpython}",
+            f"-DPython_LIBRARY={python_lib}",
+            f"-DPython_INCLUDE_DIR={python_include}",
+            f"-DPython_NumPy_INCLUDE_DIR={numpy_include_dir}",
+            _env=build_env)
         command = sh.Command("make")
-        shprint(command,
-                "-C",
-                "_build_python",
-                "-j",
-                "swigfaiss",
-#                "swigfaiss_avx2",
-                _env=build_env)
+        shprint(
+            command,
+            "-C",
+            "_build_python",
+            "-j",
+            "swigfaiss",
+            # "swigfaiss_avx2",
+            _env=build_env)
 
         super(FaissRecipe, self).build_arch(arch)
 
     def install(self):
-        arch = self.filtered_archs[0]
-        name = self.name
-        env = self.get_recipe_env(arch)
+        arch = list(self.filtered_archs)[0]
         build_dir = join(self.get_build_dir(arch.arch), "_build_python")
         os.chdir(build_dir)
         hostpython = sh.Command(self.ctx.hostpython)
-
-        shprint(
-            hostpython,
-            "setup.py",
-            "install",
-            "--single-version-externally-managed",
-            "--record=record.txt",
-            "-O2",
-            "--root",
-            self.ctx.python_prefix,
-            "--prefix",
-            "",
-            _env=env,
-        )
+        build_env = arch.get_env()
+        dest_dir = join(self.ctx.dist_dir, "root", "python")
+        build_env['PYTHONPATH'] = self.ctx.site_packages_dir
+        shprint(hostpython,
+                "setup.py",
+                "install",
+                "--single-version-externally-managed",
+                "--record=record.txt",
+                "--prefix",
+                dest_dir,
+                _env=build_env)
 
 
 recipe = FaissRecipe()
